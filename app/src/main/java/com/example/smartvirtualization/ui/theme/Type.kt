f30477128784a14a9ec7021cadb8c3c46bff117f
AkinAb55 <adada@tutamail.com>
2026-06-19 11:43:29 +0300

    Update Type.kt

--- app/src/main/java/com/example/smartvirtualization/ui/theme/Type.kt
+++ app/src/main/java/com/example/smartvirtualization/ui/theme/Type.kt
@@ -4,7 +4,7 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.sp   // импорт для sp
 
 val Typography = Typography(
     bodyLarge = TextStyle(
