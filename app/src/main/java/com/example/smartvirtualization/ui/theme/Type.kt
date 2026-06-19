1e7a6c3b7e94c8eed7348fbb2d9ebd5cb2d559a2
AkinAb55 <adada@tutamail.com>
2026-06-19 11:55:54 +0300

    Update Type.kt

--- app/src/main/java/com/example/smartvirtualization/ui/theme/Type.kt
+++ app/src/main/java/com/example/smartvirtualization/ui/theme/Type.kt
@@ -3,8 +3,8 @@ package com.example.smartvirtualization.ui.theme
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight   // импорт для FontWeight
-import androidx.compose.ui.unit.sp              // импорт для sp
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp   // импорт для sp
 
 val Typography = Typography(
     bodyLarge = TextStyle(
